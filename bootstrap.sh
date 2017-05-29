a95b47a26e5c386932befbcc10b3d4bbd9bf0045
Siddharth <sidharth3930@gmail.com>
2017-05-29 12:01:54 +0530

    add .vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,10 @@ function doIt() {
                 --exclude "bootstrap.sh" \
                 --exclude "README.md" \
                 --exclude "LICENSE-MIT.txt" \
-                -avh --no-perms .vimrc .aliases .bash_profile .bash_prompt .path, .exports  ~;
+                -avh --no-perms .vim .vimrc .aliases .bash_profile .bash_prompt .path, .exports  ~;
         source ~/.bash_profile;
+        # Install Vundle for managing vim dependencies. (hmm, where should this be??)
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
