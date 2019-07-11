d6f6d81c24f0d4b4516e67fbdff94aa343b8f876
Siddharth <sidharth3930@gmail.com>
2019-07-11 13:37:26 -0500

    single bash history

--- cp-from-home.sh
+++ cp-from-home.sh
@@ -1,4 +1,4 @@
-for file in ~/.{gitconfig,git-completion.bash,vimrc,vimrc.bundles,aliases,bash_profile,bash_prompt,path,exports}; do
+for file in ~/.{gitconfig,git-completion.bash,vimrc,vimrc.bundles,aliases,bash_profile,bash_prompt,path,exports,extra}; do
   cp $file .
 done
 
