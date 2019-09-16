ab4f577a86ccd52b70a1e5662d53b8737f3394cb
Siddharth <sidharth3930@gmail.com>
2019-09-16 21:53:17 +0000

    add user flag to virtualenv

--- virtualenv_setup.sh
+++ virtualenv_setup.sh
@@ -1,6 +1,6 @@
 # Install virtualenv, nice guide: http://python-guide-pt-br.readthedocs.io/en/latest/dev/virtualenvs/
-echo pip install virtualenv
-echo pip install virtualenvwrapper
+echo pip install virtualenv --user
+echo pip install virtualenvwrapper --user
 echo source /usr/local/bin/virtualenvwrapper.sh
 echo mkvirtualenv global --python="/usr/local/bin/python3"
 echo touch ~/.env
