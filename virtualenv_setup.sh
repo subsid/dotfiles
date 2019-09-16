40c4f6adeaef75c5df8f111c09426a339ca93e0d
Siddharth <sidharth3930@gmail.com>
2019-09-16 16:27:01 +0000

    Make virtualenv do nothing

--- virtualenv_setup.sh
+++ virtualenv_setup.sh
@@ -1,8 +1,8 @@
 # Install virtualenv, nice guide: http://python-guide-pt-br.readthedocs.io/en/latest/dev/virtualenvs/
-pip install virtualenv
-pip install virtualenvwrapper
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv global --python="/usr/local/bin/python3"
-touch ~/.env
+echo pip install virtualenv
+echo pip install virtualenvwrapper
+echo source /usr/local/bin/virtualenvwrapper.sh
+echo mkvirtualenv global --python="/usr/local/bin/python3"
+echo touch ~/.env
 echo "source ~/.virtualenvs/global/bin/activate" > ~/.env
 
