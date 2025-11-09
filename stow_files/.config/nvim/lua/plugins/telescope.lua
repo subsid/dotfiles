e45b68e2063002e7509c1b10f1fa706757891a5b
Siddharth <ssubramaniyam@etsy.com>
2025-11-09 14:50:46 -0500

    Arch setup script

--- stow_files/.config/nvim/lua/plugins/telescope.lua
+++ stow_files/.config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ M = {
     { "nvim-lua/plenary.nvim" },
     {
       "nvim-telescope/telescope-fzf-native.nvim",
-      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release && cmake --install build --prefix build",
     },
     {
       "nvim-telescope/telescope-live-grep-args.nvim",
